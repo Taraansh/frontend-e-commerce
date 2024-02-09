04c4fb5c8cee0c92b3ad039130671d8744f9a8e1
Taraansh <taraanshnandwani123@gmail.com>
2024-02-09 12:09:29 +0530

    Set up Proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: { esmExternals: true },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:3100/api/v1/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;

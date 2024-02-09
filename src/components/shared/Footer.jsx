2edba42d08f3e4a49d809b961ab7fd53e062ba2e
Taraansh <taraanshnandwani123@gmail.com>
2024-02-09 12:14:23 +0530

    Footer

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <hr/>
+    <h5 className='footerText'>Copyright &#169; 2024 E-commerce. All rights reserved.</h5>
+    </>
+  )
+}
+
+export default Footer

8d8925310ee0af4efe8227ef904e4da54abfcb95
Taraansh <taraanshnandwani123@gmail.com>
2024-02-09 12:14:13 +0530

    Head tag details

---
+++ src/components/shared/Heading.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Heading = () => {
+  return (
+    <Head>
+      <title>E-commerce</title>
+      <meta name="description" content="E-commerce - Get Instant License in a click"/>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+  );
+};
+
+export default Heading;

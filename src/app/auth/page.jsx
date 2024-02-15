3e67602b36d309dc6f989800c57869711bc6b203
Taraansh <taraanshnandwani123@gmail.com>
2024-02-15 13:00:38 +0530

    Added RegisterLogin Component

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,24 +1,30 @@
 "use client";
 import RegisterLogin from "@/components/auth/RegisterLogin";
 import React from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import { Row, Button } from "react-bootstrap";
 
 const Auth = () => {
   const [typeOfForm, setTypeOfForm] = React.useState(false);
   return (
     <>
       <Row>
+        <RegisterLogin
+          isRegisterForm={typeOfForm}
+          setTypeOfForm={setTypeOfForm}
+        />
         <Button
-        className="mx-auto my-2"
+          className="mx-auto my-2"
           variant="outline-secondary"
           id="button-addon2"
-          style={{width: 'fit-content'}}
-          onClick={() => {setTypeOfForm(!typeOfForm)
+          style={{ width: "fit-content" }}
+          onClick={() => {
+            setTypeOfForm(!typeOfForm);
           }}
         >
-          {!typeOfForm ? "New User? Register" : "Already have an account? Login"}
+          {!typeOfForm
+            ? "New User? Register"
+            : "Already have an account? Login"}
         </Button>
-        <RegisterLogin isRegisterForm={typeOfForm} />
       </Row>
     </>
   );

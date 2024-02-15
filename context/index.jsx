68da250fb33918372662844b94ac5473bf56f308
Taraansh <taraanshnandwani123@gmail.com>
2024-02-15 12:50:16 +0530

    Added states and context

--- context/index.jsx
+++ context/index.jsx
@@ -1,32 +1,25 @@
-'use client';
-import { useReducer, createContext, useEffect } from "react";
+"use client";
+import React, { useReducer, createContext, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-
-// type Props = {
-//   children: React.ReactNode;
-// };
+import { toast } from "react-toastify";
 
 const initialState = {
   user: null,
 };
 
-// type Context = {
-//   state: Record<string, any>;
-//   dispatch: (action: { type: string; payload: any }) => void;
-// };
-
 const initialContext = {
   state: initialState,
   dispatch: () => null,
+  cartItems: [],
+  cartDispatch: function (action) {
+    throw new Error("Function is not implemented");
+  },
 };
 
 const Context = createContext(initialContext);
 
-const userReducer = (state, action
-//   state: Record<string, any>,
-//   action: { type: string; payload: any }
-) => {
+const userReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
       return {
@@ -48,9 +41,78 @@ const userReducer = (state, action
   }
 };
 
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      const cartItems = [...state, action.payload];
+      window.localStorage.setItem(
+        "_e_commerce_cart",
+        JSON.stringify(cartItems)
+      );
+      return cartItems;
+
+    case "REMOVE_FROM_CART":
+      const newCartItems = state.filter((item) => {
+        item.skuId !== action.payload?.skuId;
+      });
+      window.localStorage.setItem(
+        "_e_commerce_cart",
+        JSON.stringify(newCartItems)
+      );
+      return newCartItems;
+
+    case "UPDATE_CART":
+      const updatedCartItems = state.map((item) => {
+        if (item.skuId === action.payload?.skuId) {
+          return action.payload;
+        }
+        return item;
+      });
+      window.localStorage.setItem(
+        "_e_commerce_cart",
+        JSON.stringify(updatedCartItems)
+      );
+      return updatedCartItems;
+
+    case "GET_CART":
+      return action.payload;
+
+    case "CLEAR_CART":
+      window.localStorage.removeItem("_e_commerce_cart");
+      return [];
+
+    default:
+      return state;
+  }
+};
+
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, initialState);
+  const [cartItems, cartDispatch] = useReducer(cartReducer, []);
+  const [products, setProducts] = React.useState({});
+  const [metadata, setMetaData] = React.useState({});
+
   const router = useRouter();
+
+  const fetchProducts = async (type, search) => {
+    try {
+      // console.log(type, search)
+      const url = `${process.env.NEXT_PUBLIC_BASE_API_URL}/products${
+        type ? `?${type}=${search}` : ""
+      }`;
+      const { data } = await axios.get(url);
+      setProducts(data.result.products);
+      setMetaData(data.result.metadata);
+      // console.log(data.result);
+    } catch (error) {
+      // console.log(error);
+      toast.error("Something Went wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   useEffect(() => {
     dispatch({
@@ -59,6 +121,11 @@ const Provider = ({ children }) => {
         ? JSON.parse(localStorage.getItem("_e_commerce_user") || "{}")
         : null,
     });
+
+    const cartItems = JSON.parse(
+      window.localStorage.getItem("_e_commerce_cart") || "[]"
+    );
+    cartDispatch({ type: "GET_CART_ITEMS", payload: cartItems });
     return;
   }, []);
 
@@ -67,7 +134,8 @@ const Provider = ({ children }) => {
       return response;
     },
     (error) => {
-      if (error.response.status === 401) {
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
         return new Promise((resolve, reject) => {
           axios
             .put("/api/v1/users/logout")
@@ -97,7 +165,21 @@ const Provider = ({ children }) => {
   }, []);
 
   return (
-    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+    <Context.Provider
+      value={{
+        state,
+        dispatch,
+        cartItems,
+        cartDispatch,
+        products,
+        setProducts,
+        metadata,
+        setMetaData,
+        fetchProducts,
+      }}
+    >
+      {children}
+    </Context.Provider>
   );
 };
 

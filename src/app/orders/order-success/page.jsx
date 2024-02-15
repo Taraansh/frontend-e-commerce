9fee392d11a0bb34b748a0ae975aa5b1c5e05d18
Taraansh <taraanshnandwani123@gmail.com>
2024-02-15 13:02:03 +0530

    Page to display if order is successful

---
+++ src/app/orders/order-success/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useContext, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Context } from "../../../../context";
+
+const OrderSuccess = () => {
+  const router = useRouter();
+  const {
+    state: { user },
+  } = useContext(Context);
+  const { cartDispatch } = useContext(Context);
+  useEffect(() => {
+    cartDispatch({ type: "CLEAR_CART", payload: {} });
+  }, [router, user, cartDispatch]);
+  return (
+    <>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <div className="jumbotron text-center">
+            <h1 className="display-3">Thank You!</h1>
+            <p className="lead">
+              <strong>Please check your order details</strong> for further
+              instructions. You will recieve an email with order details.
+            </p>
+            <hr />
+            <p className="lead">
+              <Link
+                href={`/products`}
+                className="btn btn-primary btn-sm"
+                role="button"
+              >
+                Shop More
+              </Link>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderSuccess;

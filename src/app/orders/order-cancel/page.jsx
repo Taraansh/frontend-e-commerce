8bb25d931f9974fe2476554eeefbbd95f9004cd8
Taraansh <taraanshnandwani123@gmail.com>
2024-02-15 13:01:44 +0530

    Page to display if order cancels

---
+++ src/app/orders/order-cancel/page.jsx
@@ -0,0 +1,48 @@
+"use client";
+import { Context } from "../../../../context";
+import Link from "next/link";
+import React, { useContext, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useRouter } from "next/navigation";
+
+const OrderCancel = () => {
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user || !user.email) {
+      router.push("/");
+    }
+  }, [router, user]);
+
+  return (
+    <>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <div className="jumbotron text-center">
+            <h1 className="display-3 text-danger">Opps! Cancelled !</h1>
+            <p className="lead">
+              <strong>Payment failed !</strong> Your order got cancelled. Please
+              try again.
+            </p>
+            <hr />
+            <p className="lead">
+              <Link
+                href="/products"
+                className="btn btn-secondary btn-sm"
+                role="button"
+              >
+                Shop More
+              </Link>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderCancel;
